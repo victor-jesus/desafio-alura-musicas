60ed3ea43cd52dfbbbac7f22ff2ab0e9677805f8
victor-jesus <victorgustavo191@gmail.com>
2025-07-20 01:51:42 -0300

    first commit

---
+++ src/main/java/com/victorjesus/projeto_desafio_musicas/ProjetoDesafioMusicasApplication.java
@@ -0,0 +1,13 @@
+package com.victorjesus.projeto_desafio_musicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDesafioMusicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDesafioMusicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorjesus/projeto_desafio_musicas/ProjetoDesafioMusicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorjesus.projeto_desafio_musicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDesafioMusicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
